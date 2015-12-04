7701902ce801c92b28e9b2c83820fed8e8682daf
root <root@localhost.localdomain>
2015-12-04 17:30:00 -0500

    Added file to initialize some stuff on Kali

---
+++ init_kali.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Shitload of stuff. Many are specific to Kali Linux v2
+
+# Installing zsh, customize .zshrc
+sudo apt-get install zsh
+if [ ! -e $HOME/.oh-my-zsh/ ]
+then
+	sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+	echo -n "source $HOME/.aliases" >> $HOME/.zshrc
+fi
+
+# Settings some aliases
+echo 'alias tmp="cd /tmp"' >> $HOME/.aliases
+
+# Setting locale
+echo -n "setxkmap fr" >> $HOME/.zshrc
+
+# Adding impacket examples to executables
+chmod +x /usr/share/doc/python-impacket/examples/*
+ln -s /usr/share/doc/python-impacket/examples/ /usr/local/bin/python-impacket/
+
+# Updating
+apt-get update
+apt-get upgrade

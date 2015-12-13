66d60dcc4d7085315543339d01b0ee2041bffb97
Arnaud Abramovici <arnaud@ruuand.fr>
2015-12-13 15:53:36 -0500

    Fixes iprange.py

--- iprange.py
+++ iprange.py
@@ -5,7 +5,7 @@ from netaddr import *
 import sys
 
 def get_cidrs(ip_start, ip_end):
-    return ', '.join([str(range) for range in netaddr.iprange_to_cidrs(ip_start, ip_end)])
+    return ', '.join([str(range) for range in iprange_to_cidrs(ip_start, ip_end)])
 
 if __name__ == '__main__':
     if len(sys.argv) == 3:

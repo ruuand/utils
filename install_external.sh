0d0cb83b11a629bd58a0fcbd2ceb122bf7f9b117
Arnaud Abramovici <arnaud@ruuand.fr>
2016-01-22 15:06:44 -0500

    Removes webkit2png

--- install_external.sh
+++ install_external.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 # This script installs several tools:
 
-# Installs python-webkit2png
-cd python-webkit2png
-./setup.py install
-cd ..
-
 # Install discover scripts
 git clone https://github.com/leebaird/discover.git /opt/discover
 cd /opt/discover && ./update.sh

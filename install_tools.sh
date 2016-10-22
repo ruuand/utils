c8f42f53430ff4bc71ad99bfa18cf9491a2e04e2
root <root@localhost.localdomain>
2016-10-22 17:51:56 -0400

    Modification

--- install_tools.sh
+++ install_tools.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 # This script installs several utils:
 # - Adds a symbolic from /usr/local/bin to the current repo
-# - Adds an alias for each each script
-#
 
 target_dir=/usr/local/bin
 
-files="tools/nessus_merger.py tools/parse_nessus_xml.pl tools/iptables-monitor.sh\
-    tools/ftpserver.py"
+files="tools/nessus_merger.py tools/parse_nessus_xml.pl tools/ftpserver.py \
+	tools/script.sh tools/webkit2png-multi.sh"
 
 # Installs some prerequisites for parse_nessus_xml.pl
 cpan install XML::TreePP Data::Dumper Math::Round Excel::Writer::XLSX\
     Data::Table Excel::Writer::XLSX::Chart
 
-# Adds each file to /usr/local/bin and creates an alias (pentest is the alias
-# for pentest.sh, iprange the alias for iprange.py, etc.
+# Adds each file to /usr/local/bin
 for path in $files
 do
     file=`basename $path`
@@ -24,7 +21,4 @@ do
         unlink $target_dir/$file
     fi
     ln -s `pwd`/$path $target_dir/$file
-    no_ext=`echo $file | cut -d'.' -f1`
-    grep -q "alias $no_ext=$file" $HOME/.aliases || \
-        echo "alias $no_ext=$file" >> $HOME/.aliases
 done
